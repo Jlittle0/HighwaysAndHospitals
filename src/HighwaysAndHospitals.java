7ab75248c6b793e136852703ebb6d1a9197122eb
Josh Little <josh.little@menloschool.org>
2024-09-11 19:54:00 -0700

    Commit #3 after 1 hour work session outside of class. Currently works but is extremely slow/inefficient so want to improve that later. Wrote the stuff I did in work long.

--- src/HighwaysAndHospitals.java
+++ src/HighwaysAndHospitals.java
@@ -38,76 +38,82 @@ public class HighwaysAndHospitals {
 
 
     public static long cost(int n, int hospitalCost, int highwayCost, int cities[][]) {
+
         // Print Statements
         Arrays.sort(cities, (a, b) -> Integer.compare(a[0],b[0]));
-        ArrayList<int[]> city = new ArrayList<int[]>(Arrays.asList(cities));
-//        System.out.println("total number of citites" + n);
-//        System.out.println("Hospital Cost: " + hospitalCost);
-//        System.out.println("Highway Cost: " + highwayCost);
-        System.out.println("Cities: " + Arrays.deepToString(cities));
-//        System.out.println("City: " + city.get(0)[1]);
 
         // If the hospital cost is lower than that of a highway, just build a hospital everywhere.
         if (hospitalCost < highwayCost)
-            return n * hospitalCost;
+            return Long.valueOf(n) * hospitalCost;
 
         // Array to hold all the visited cities with a value of 0 for not and 1 for visited.
         int[] visited = new int[n + 1];
         int currentNode;
         int clusters = 0;
-        int count = 0;
-        int first;
-        int second;
         Queue<Integer> path = new LinkedList<>();
         while (!checkVisited(visited)) {
             clusters++;
-            // Interate through the entire visited list, find the first element that hasn't been
-            // visited, and then grab the first city where that element appears
             for (int i = 1; i < visited.length; i++)
                 if (visited[i] == 0) {
-                    for (int j = 0; i < cities.length; i++) {
-                        if (cities[j][0] == i) {
-                            path.add(cities[j][0]);
-                            count = j;
-                            first = j;
-                            second = 0;
-                            break;
-                        } else if (cities[j][1] == 1) {
-                            path.add(cities[j][1]);
-                            count = j;
-                            first = 0;
-                            second = j;
-                            break;
-                        }
-                    }
+                    path.add(i);
                     visited[i] = 1;
                     break;
                 }
             // Run BFS on the first element that's not visited
             while (!path.isEmpty()) {
+                boolean found = false;
+                boolean foundSecond = false;
+                int count = 0;
+                int temp = 0;
+                Arrays.sort(cities, (a, b) -> Integer.compare(a[0], b[0]));
                 currentNode = path.remove();
                 visited[currentNode] = 1;
-
                 for (int i = 0; i < cities.length; i++) {
-                    if (cities[i][0] == currentNode || cities[i][1] == currentNode) {
+                    if (cities[i][0] == currentNode) {
                         count = i;
+                        found = true;
                         break;
                     }
                 }
-                int temp = 0;
-                while (cities[count + temp][0] == currentNode && count + temp < cities.length - 1) {
-                    System.out.println("check " + currentNode);
-                    // If the connection hasn't been visited
-                    if (visited[cities[count + temp][1]] == 0) {
-                        path.add(cities[count + temp][1]);
-                        visited[cities[count + temp][1]] = 1;
+                if (found) {
+                    while (cities[count + temp][0] == currentNode) {
+                        // If the connection hasn't been visited
+                        if (visited[cities[count + temp][1]] == 0) {
+                            path.add(cities[count + temp][1]);
+                            visited[cities[count + temp][1]] = 1;
+                        }
+                        if (count + temp == cities.length - 1)
+                            break;
+                        temp++;
                     }
-                    temp++;
                 }
-                System.out.println("Path size: " + path.size());
+                Arrays.sort(cities, (a, b) -> Integer.compare(a[1], b[1]));
+                for (int i = 0; i < cities.length; i++) {
+                    if (cities[i][1] == currentNode) {
+                        count = i;
+                        foundSecond = true;
+                        break;
+                    }
+                }
+                if (foundSecond) {
+                    temp = 0;
+                    while (cities[count + temp][1] == currentNode) {
+                        // If the connection hasn't been visited
+                        if (visited[cities[count + temp][0]] == 0) {
+                            path.add(cities[count + temp][0]);
+                            visited[cities[count + temp][0]] = 1;
+                        }
+                        if (count + temp == cities.length - 1)
+                            break;
+                        temp++;
+                    }
+                }
             }
         }
-        System.out.println(clusters);
+        // The current problem is that the system only takes into account the first connection
+        // between two cities rather than the second one so if 4 connected to 6 and 5 to 6,
+        // five is considered a new cluster as it's not connected to anything else in the 2nd
+        // connection of an edge despite actually being connected to 6 which is connected to 4
         return clusters * hospitalCost + ((n - clusters) * highwayCost);
     }
 }

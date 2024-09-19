911839f8a1e2d0ba21a3062d6bd49af39ccaa2d9
Josh Little <josh.little@menloschool.org>
2024-09-19 12:46:02 -0700

    Final Submission. Comments added and solution works.

--- src/HighwaysAndHospitals.java
+++ src/HighwaysAndHospitals.java
@@ -20,27 +20,10 @@ public class HighwaysAndHospitals {
      *  hospital access for all citizens in Menlo County.
      */
 
-    // Idea: To calculate the final price, all you need to do is find the number of indepedent
-    // clusters that don't connect to eachother, and then perform the math of
-    // (clusters * hospital price) + (cities-clusters*road price) = Final price.
-    // TO do this, I'm thinking of running BFS on the first in the 2D cities array and then creating
-    // another array that holds a number corresponding to each of the cities where I can
-    // mark something as visited and not visited. If something is not visited by the time
-    // BFS no longer has any elements to add, start a new search with that element as
-    // the first element in the queue.
-
-    public static boolean checkVisited(int[] visited) {
-        for (int i = 1; i < visited.length; i++)
-            if (visited[i] == 0)
-                return false;
-        return true;
-    }
-
-
     public static long cost(int n, int hospitalCost, int highwayCost, int cities[][]) {
-
-        // Print Statements
-        Arrays.sort(cities, (a, b) -> Integer.compare(a[0],b[0]));
+        // Unneccesary but wanted to keep this sorting bc it was part of original solution
+        // and is a helpful visualization whenever I need to print out the cities array for testing.
+//        Arrays.sort(cities, (a, b) -> Integer.compare(a[0],b[0]));
 
         // If the hospital cost is lower than that of a highway, just build a hospital everywhere.
         if (hospitalCost < highwayCost)
@@ -48,32 +31,45 @@ public class HighwaysAndHospitals {
 
         // Array to hold the root of each city and a zero if it's its own root
         int[] roots = new int[n + 1];
-        int currentNode;
+        // Variables to store the first and second city connected between an edge as well as
+        // copies of said variables to rerun through the path and implement path compression.
         int firstCity, firstCityCopy, secondCity, secondCityCopy, temp;
+        // For every edge...
         for (int i = 0; i < cities.length; i++) {
+            // Assign varaibles to actual city values
             firstCity = cities[i][0];
             secondCity = cities[i][1];
             firstCityCopy = cities[i][0];
             secondCityCopy = cities[i][1];
+            // While the first element of the edge's parent isn't a root, keep traversing up tree
             while (roots[firstCity] > 0) {
                 firstCity = roots[firstCity];
             }
+            // After finding the root, then go back through until everything in the previous path
+            // is pointing to it
             while (roots[firstCityCopy] != firstCity && firstCity <= 0) {
                 temp = roots[firstCityCopy];
                 roots[firstCityCopy] = firstCity;
                 firstCityCopy = temp;
             }
+            // Repeat the above processes on the second element connected by the current edge
             while (roots[secondCity] > 0) {
                 secondCity = roots[secondCity];
             }
+            // Same as above
             while (roots[secondCityCopy] != secondCity && secondCity <= 0) {
                 temp = roots[secondCityCopy];
                 roots[secondCityCopy] = secondCity;
                 secondCityCopy = temp;
             }
+            // After all paths are compressed and roots found, check which is greater or if the
+            // two elements of the edge share the same root. If they do share, then do nothing.
+            // If the first is bigger, have it inheret the second and vice versa.
             if (firstCity == secondCity)
                continue;
+            // Subtracting an extra -1 to include the root that isn't held in its own value in map.
             else if (roots[firstCity] <= roots[secondCity]) {
+                // Since the orders are stored as negative values, subtract one to include root
                 roots[firstCity] += roots[secondCity] - 1;
                 roots[secondCity] = firstCity;
             }
@@ -83,15 +79,14 @@ public class HighwaysAndHospitals {
             }
         }
 
+        // Find the number of independent graphs by looking for the # of roots (negative or 0)
         int clusters = 0;
         for (int i = 1; i < roots.length; i++) {
             if (roots[i] <= 0)
                 clusters++;
         }
-        // The current problem is that the system only takes into account the first connection
-        // between two cities rather than the second one so if 4 connected to 6 and 5 to 6,
-        // five is considered a new cluster as it's not connected to anything else in the 2nd
-        // connection of an edge despite actually being connected to 6 which is connected to 4
+
+        // Return the price of everything!
         return Long.valueOf(clusters) * hospitalCost + ((n - clusters) * highwayCost);
     }
 }
